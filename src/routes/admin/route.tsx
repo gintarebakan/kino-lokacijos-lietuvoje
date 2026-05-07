63d6fc71a617a7e56fcd7cc5b548fea724e26f81
gintare-rgb <75936748+gintare-rgb@users.noreply.github.com>
2026-05-08 02:36:16 +0300

    fix: bypass main layout for admin routes

--- src/routes/admin/route.tsx
+++ src/routes/admin/route.tsx
@@ -58,7 +58,7 @@ export default function AdminLayout() {
         flexShrink: 0,
         position: "fixed",
         top: 0,
-        left: 0,
+        left: 72,
         height: "100dvh",
       }}>
         <div style={{ padding: "24px 20px 16px" }}>
