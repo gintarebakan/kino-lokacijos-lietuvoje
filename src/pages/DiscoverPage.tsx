1de01c1fcbef5428439915c545fd41e1a6f034f7
gintare-rgb <75936748+gintare-rgb@users.noreply.github.com>
2026-05-17 20:20:21 +0300

    fix

--- src/pages/DiscoverPage.tsx
+++ src/pages/DiscoverPage.tsx
@@ -196,7 +196,7 @@ export default function DiscoverPage() {
         useMapStore.getState().setPendingLocation(slug);
         useMapStore.getState().setSelectedLocationFromDiscover(slug);
         // Force zoom to 15 to ensure individual markers are visible (not clustered)
-        useMapStore.getState().setForceZoom(15);
+        useMapStore.getState().setForceZoom(13);
       }, 150);
     }
   };
